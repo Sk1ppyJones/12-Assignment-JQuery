bacb9d28f6eecb0692d8c6e15d5abfb1e7fe8ce3
Casey Jones <caseysemail52@gmail.com>
2024-11-14 15:06:11 -0700

    Done

--- JQuery.js
+++ JQuery.js
@@ -1,3 +1,17 @@
 $(document).ready(function () {
     
+    $("#show-button").click(function(){
+        $("#partial-text").toggle("slow");
+        $("#full-text").slideToggle("slow");
+
+        var currentText = $("#showmore").text();
+        if (currentText == "Show More...") {
+            $(".card-img-top").css({'filter': 'blur(5px)' });
+            $("#showmore").text("Show Less...")
+        } else {
+            $(".card-img-top").css({ 'filter': 'blur(0px)'})
+            $("#showmore").text("Show More...")
+        }
+    });
+
 });
