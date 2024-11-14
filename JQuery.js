1c8653a26b7afdada8844277f795a340c6ef0eb9
Casey Jones <caseysemail52@gmail.com>
2024-11-14 15:31:17 -0700

    Update JQuery.js

--- JQuery.js
+++ JQuery.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+
     $("#show-button").click(function () {
         $("#partial-text").toggle("slow");
         $("#full-text").slideToggle("slow");
@@ -6,10 +7,10 @@ $(document).ready(function () {
         var currentText = $("#showmore").text();
         if (currentText == "Show More...") {
             $(".card-img-top").css({ 'filter': 'blur(5px)' });
-            $("#showmore").text("Show Less...")
+            $("#showmore").text("Show Less...");
         } else {
-            $(".card-img-top").css({ 'filter': 'blur(0px)' })
-            $("#showmore").text("Show More...")
+            $(".card-img-top").css({ 'filter': 'blur(0px)' });
+            $("#showmore").text("Show More...");
         }
     });
 
