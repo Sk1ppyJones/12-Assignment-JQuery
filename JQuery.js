5f2dd08ab71c6f00ec2087d404c5050e1602508f
Casey Jones <caseysemail52@gmail.com>
2024-11-14 15:20:54 -0700

    Fix?

--- JQuery.js
+++ JQuery.js
@@ -1,15 +1,19 @@
 $(document).ready(function () {
-    
-    $("#show-button").click(function(){
+    $.getJSON("/data.json", function (data) {
+        console.log(data);
+        $("#text").html(data["text"]);
+    });
+
+    $("#show-button").click(function () {
         $("#partial-text").toggle("slow");
         $("#full-text").slideToggle("slow");
 
         var currentText = $("#showmore").text();
         if (currentText == "Show More...") {
-            $(".card-img-top").css({'filter': 'blur(5px)' });
+            $(".card-img-top").css({ 'filter': 'blur(5px)' });
             $("#showmore").text("Show Less...")
         } else {
-            $(".card-img-top").css({ 'filter': 'blur(0px)'})
+            $(".card-img-top").css({ 'filter': 'blur(0px)' })
             $("#showmore").text("Show More...")
         }
     });
